673476365bb18bb653fc6e42c4c9a1152df908c3
David Barts <dbarts@seomoz.org>
2013-01-10 13:39:24 -0800

    Update tests for new code.

--- test.py
+++ test.py
@@ -68,13 +68,14 @@ class Test(unittest.TestCase):
         '''Make sure we escape paths correctly'''
         examples = [
             ('hello%20and%20how%20are%20you', 'hello%20and%20how%20are%20you'),
-            ('danny\'s pub'                 , 'danny%27s%20pub'              ),
             ('danny%27s pub?foo=bar&yo'     , 'danny%27s%20pub?foo=bar&yo'   ),
             # Thanks to @myronmarston for these test cases
             ('foo?bar none=foo bar'         , 'foo?bar%20none=foo%20bar'     ),
             ('foo;a=1;b=2?a=1&b=2'          , 'foo;a=1;b=2?a=1&b=2'          ),
             ('foo?bar=["hello","howdy"]'    ,
                 'foo?bar=%5B%22hello%22,%22howdy%22%5D'),
+            (u'española,nm%2cusa.html?gunk=junk+glunk&foo=bar baz',
+                'espa%C3%B1ola,nm%2Cusa.html?gunk=junk+glunk&foo=bar%20baz')
         ]
 
         base = 'http://testing.com/'
@@ -84,6 +85,15 @@ class Test(unittest.TestCase):
             self.assertEqual(url.parse(bad).escape().utf8(), good)
             # Escaping should also be idempotent
             self.assertEqual(url.parse(bad).escape().escape().utf8(), good)
+
+        # "'" is a sub-delim and is not equivalent to %27 per RFC 3986!
+        more_examples = [
+            ('danny\'s pub'                 , 'danny%27s%20pub'              ),
+        ]
+        for bad, good in more_examples:
+            bad = base + bad
+            good = base + good
+            self.assertNotEqual(url.parse(bad).escape().utf8(), good)
 
     def test_equiv(self):
         '''Make sure equivalent urls work correctly'''
