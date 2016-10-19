e3eaee57988be742fdf46d22154c44cf210d0371
Dan Lecocq <dan@moz.com>
2016-10-19 14:38:29 -0700

    Bump version to 0.3.0.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ ext_modules = [
 
 setup(
     name             = 'url',
-    version          = '0.3.0rc2',
+    version          = '0.3.0',
     description      = 'URL Parsing',
     long_description = '''
 Some helper functions for parsing URLs, sanitizing them, normalizing them.
