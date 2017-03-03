3b2e3051e17465ce49b706fc57a31cd1d9b0595d
Brandon Forehand <brandon@moz.com>
2017-03-03 14:54:16 -0800

    Automatically activate virtualenv on `vagrant ssh`.
    Also, automatically change to correct directory on login.

--- scripts/vagrant/provision.sh
+++ scripts/vagrant/provision.sh
@@ -40,4 +40,9 @@ pushd /vagrant
     # Lastly, our dependencies
     pip install -r requirements.txt
 
+    echo '
+    cd /vagrant
+    # Activate virtualenv
+    . /vagrant/venv/bin/activate
+    ' >> ~/.bash_profile
 popd
